b6dc9f96e4ed85b1e8f9e02647543c5d0430aca4
Glitch (brave-absorbed-petroleum) <none>
2021-01-31 07:04:42 +0000

    💵💄 Checkpoint ./public/index.html:27683896/117 ./public/script.js:27683896/1943

--- public/script.js
+++ public/script.js
@@ -1,7 +1,15 @@
 /*
   Project:   Hands-On Project 1-1
-	Author:    
-	Date:      
+	Author:    Guillermo Molina Matus 
+	Date:      1/31/21
 	Purpose:   To demonstrate the basic structure of a web project.
 */
 
+
+  document.write("<ol>");
+  document.write("<li>Reduce spending on non-necessities.</li>");
+  document.write("<li>Use extra money to pay off debt, starting with the highest-interest credit card.</li>");
+  document.write("<li>Continue paying off debts until you are debt free.</li>");
+  document.write("<li>Put a fixed percent of your pay aside in savings.</li>");
+  document.write("<li><a href='https://investor.vanguard.com/advice/compare-financial-advice?cmpgn=RIG:PS:PSRXX:ADVGRW:05202020:XXX:XXX:ADV_2020:XXX:XXXX:XXX:GOO:Tier1:FinPlan:FinPlan&s_kwcid=AL!11156!3!439366368655!e!!g!!financial%20planning&gclid=CjwKCAiApNSABhAlEiwANuR9YDqFV3m6kx5nEO2U-nuZsFlCn6l0vQHr4OjzoYBePjPeX3YnFaZ7thoCwv0QAvD_BwE'>https://investor.vanguard.com/advice/compare-financial-advice?cmpgn=RIG:PS:PSRXX:ADVGRW:05202020:XXX:XXX:ADV_2020:XXX:XXXX:XXX:GOO:Tier1:FinPlan:FinPlan&s_kwcid=AL!11156!3!439366368655!e!!g!!financial%20planning&gclid=CjwKCAiApNSABhAlEiwANuR9YDqFV3m6kx5nEO2U-nuZsFlCn6l0vQHr4OjzoYBePjPeX3YnFaZ7thoCwv0QAvD_BwE </a></li>")
+  document.write("</ol>");
